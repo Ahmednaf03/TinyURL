fcb2d06ec740ac97928587ec14aa52d6d97b6c9d
Ahmednaf03 <ahamednafees777@gamil.com>
2025-11-21 16:01:20 +0530

    init--

---
+++ Backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+console.log("CWD:", process.cwd());
+console.log("ENV:", process.env.DB_URL);
+
+
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// --- DB connection (Neon) ---
+const pool = new Pool({
+  connectionString: process.env.DB_URL,
+  ssl: { rejectUnauthorized: false }, // Neon needs SSL
+});
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
