da4ac6641f5f68c89d6fff8171223b33eb8e04f9
Ahmednaf03 <ahamednafees777@gamil.com>
2025-11-22 21:06:00 +0530

    utils for api calls done

---
+++ Frontend/src/types/types.ts
@@ -0,0 +1,8 @@
+export interface UrlItem {
+    id : number;
+    code : string;
+    originalUrl : string;
+    shortUrl : string;
+    visitCount : number;
+    createdAt : string;
+}
---
+++ Frontend/src/utils/urls.ts
@@ -0,0 +1,27 @@
+import type { UrlItem } from "../types/types";
+const BASE = "http://localhost:4000/api/urls";
+
+export async function createUrl(longurl: string): Promise<UrlItem | {error: string}> {
+const res = await fetch(BASE,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'},
+        body: JSON.stringify({ url: longurl })
+});
+return res.json();
+}
+
+export async function getUrls(): Promise<UrlItem[]> {
+    const res =  await fetch (BASE);
+    return res.json();
+}
+
+export async function getUrlStats(code: string): Promise<UrlItem> {
+    const res = await fetch(`${BASE}/${code}`)
+    return res.json();
+}
+
+export async function deleteUrl(code: string): Promise<void>{
+    await fetch(`${BASE}/${code}`, {method: 'DELETE' });
+}
+ 
